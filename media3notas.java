4187eae75740c6c35c8216a238365e33e7ae36e6
Hamilton1602 <hamiltonjunior1602@gmail.com>
2026-04-22 23:04:45 +0000

    a

---
+++ media3notas.java
@@ -0,0 +1,12 @@
+void main() {
+    float nota1 = interger.parseInt(IO.readln("informe a nota da 1aunidade"));
+    float nota2 = integer.parseInt(IO.readln("informe a nota da 2a unidade"));
+float nota3 = integer.parseInt(IO.readln("informe a nota da 3a unidade"));
+
+float media = (nota1 + nota2 + nota3) /3
+
+if (media <= 7)
+    System.out.prinf(Aprovado com média %.2f \n", media)
+else
+    System.out.printf("Reprovado com média %.2f \n", media)
+}
