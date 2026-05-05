7cdea87e503fa76d2873ebbe525b979aadd56394
Hamilton1602 <hamiltonjunior1602@gmail.com>
2026-05-05 15:59:07 +0000

    organização

--- as.java
+++
@@ -1,6 +0,0 @@
-void main() {
-    float batman = Float.parseFloat(IO.readln("Digite o valor de Batman: "));
-    float superman = Float.parseFloat(IO.readln("Digite o valor de Superman: "));
-
-    float resultado = batman / superman + batman;
-    IO.println("O resultado é: " + resultado);
